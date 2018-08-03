e20bd35b4d9baf01f078590cdb1b3e20dee57af0
Niku Piku <nikupiku@Vos-iMac.local>
2018-08-02 23:52:12 -0700

    4th project

---
+++ 4_VegasRouletteApp/VegasRoulette/Playground_VariablesAndMore.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print(str);
+
+// Types: Int - Float - Double - Bool - String
+let wholeNos : Int = 10;
+let text : String = "Hi";
+let boolean : Bool = true;
+let decimalShort : Float = 1.2;
+let decimalLongPrecision : Double = 1.242556464;
+
+// Variables
+var myAge : Int = 32;
+myAge = 50;
+
+// Constants
+let myAge2 : Int = 100;
+// WRONG - myAge2 = 200;
+let myAge3 =  myAge2 + 100;
+let myName : String = "Nikita"
+let myFullName = myName + "Govind"; //OR let myFullName = "(\(myName) Govind)";
+// WRONG - let myAge3 =  myAge2 + "Hello"
+// But can explicitely convert any data type to String
+let nameAndAge = "(\(myName), \(myAge))";
+
+
+
+
---
+++ 4_VegasRouletteApp/VegasRoulette/VegasRoulette/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  VegasRoulette
+//
+//  Created by Niku Piku on 8/2/18.
+//  Copyright © 2018 Niku Piku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var leftDiceRandomValue: Int = 0;
+    var rightDiceRandomValue: Int = 0;
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"];
+    
+    @IBOutlet weak var leftDiceImageView: UIImageView!
+    @IBOutlet weak var rightDiceImageView: UIImageView!
+    
+    /////////////////////////////  Startup code ////////////////////////////
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    ////////////////////////////// User Actions //////////////////////////////
+    @IBAction func rollButtonPressed(_ sender: Any)
+    {
+        updateDiceImages();
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?)
+    {
+        updateDiceImages();
+    }
+    
+    ////////////////////////////// Other methods //////////////////////////////
+    func updateDiceImages() -> Void
+    {
+        //Casted // This creates random generator from 0 to 5
+        leftDiceRandomValue = Int(arc4random_uniform(6));
+        rightDiceRandomValue = Int(arc4random_uniform(6));
+        
+        print(leftDiceRandomValue, rightDiceRandomValue);
+        
+        leftDiceImageView.image = UIImage(named: diceArray[leftDiceRandomValue]);
+        rightDiceImageView.image = UIImage(named: diceArray[rightDiceRandomValue]);
+    }
+}
+
